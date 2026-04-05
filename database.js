2baf9e476ed8be9d7de532023b23d82d62e2761d
butze6455-cloud <butze6455@gmail.com>
2026-04-06 01:28:54 +0200

    Update database.js

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 const { MongoClient } = require("mongodb");
 
 // 👉 HIER DEIN MONGO LINK REIN (WICHTIG: MIT "")
-const MONGO_URI = 
+const MONGO_URI = "mongodb+srv://butze6455_db_user:OvthYec6q5ZsUzZo@cluster0.3abvojv.mongodb.net/?appName=Cluster0";
 
-const client = new MongoClient(MONGO_URI);"mongodb+srv://butze6455_db_user:OvthYec6q5ZsUzZo@cluster0.3abvojv.mongodb.net/?appName=Cluster0";
+const client = new MongoClient(MONGO_URI);
 
 let db;
 
