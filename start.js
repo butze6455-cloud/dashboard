57e22ba2d6cdf8d1ec0a93c456a5433a81a98bc3
butze6455-cloud <butze6455@gmail.com>
2026-04-06 03:10:59 +0200

    Update start.js

--- start.js
+++ start.js
@@ -1,11 +1,2 @@
-const { spawn } = require("child_process");
-
-function run(file) {
-    const p = spawn("node", [file]);
-
-    p.stdout.on("data", data => console.log(`[${file}] ${data}`));
-    p.stderr.on("data", data => console.error(`[${file} ERROR] ${data}`));
-}
-
-run("index.js");
-run("dashboard.js");
+require("./index");
+require("./dashboard");
