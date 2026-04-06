da51d84aff10b182e1acf7b3cf309404a4c978b4
butze6455-cloud <butze6455@gmail.com>
2026-04-06 03:46:51 +0200

    Update start.js

--- start.js
+++ start.js
@@ -4,7 +4,7 @@ const app = require("./dashboard");
 
 const PORT = process.env.PORT || 10000;
 
-// 🔥 GLOBAL LOCK → verhindert doppelt starten
+// 🔥 verhindert doppelt starten (Render fix)
 if (!global.serverStarted) {
     app.listen(PORT, () => {
         console.log("🌐 Dashboard läuft auf Port " + PORT);
